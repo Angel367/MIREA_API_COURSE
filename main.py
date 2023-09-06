8d719c83a7b0744101f66c401725ed543c1fec2f
Волков Егор <egor.volkov20038@gmail.com>
2023-09-07 02:01:20 +0300

    prac2 finished

--- main.py
+++ main.py
@@ -1,15 +1,62 @@
 import requests
+import time
+import matplotlib.pyplot as plt
 
-api_key = "00550c51ae2251372b0cf97e57a80fac"
-print("Введите название города: ")
-city = input()
-data = requests.get(f"http://api.weatherstack.com/current?"
-                    f"access_key={api_key}&"
-                    f"query={city}&")
-useful_data = data.json()['current']
+api_key = "ce8c8c95485e0c07bb277e1172325c78"
 
-print(f"Температура: {useful_data['temperature']}°C"
-      f"\nВлажность: {useful_data['humidity']}%"
-      f"\nДавление: {useful_data['pressure']} мм рт.ст."
-      f"\nВетер: {useful_data['wind_speed']} м/с"
-      )
+first_request = requests.get(f"http://api.currencylayer.com/historical?"
+                             f"access_key={api_key}&"
+                             f"source=USD&"
+                             f"currencies=EUR, GBP, JPY&"
+                             f"date=2018-02-22")
+
+useful_data = first_request.json()['quotes']
+
+print(f"================================\n"
+      f"Доллар к Евро: {useful_data['USDEUR']}\n"
+      f"Доллар к Фунту Стерлингу: {useful_data['USDGBP']}\n"
+      f"Доллар к Йенам: {useful_data['USDJPY']}\n")
+
+
+def generate_data():
+    from datetime import datetime, timedelta
+
+    start_date = datetime(2016, 2, 25)
+    end_date = datetime(2017, 2, 21)
+
+    current_date = start_date
+    date_list = []
+
+    while current_date <= end_date:
+        date_list.append(current_date.strftime('%Y-%m-%d'))
+        current_date += timedelta(days=1)
+    return date_list
+
+
+course = {}
+time.sleep(3)
+for date in generate_data():
+    second_request = requests.get(f"http://api.currencylayer.com/historical?"
+                                  f"access_key={api_key}&"
+                                  f"source=USD&"
+                                  f"currencies=EUR&"
+                                  f"date={str(date)}")
+    print(second_request.json())
+    time.sleep(1)
+    course[date] = second_request.json()['quotes']['USDEUR']
+
+dates = list(course.keys())
+values = list(course.values())
+
+
+plt.figure(figsize=(10, 6))  # Размер графика
+plt.plot(dates, values, marker='o', linestyle='-', color='b', markersize=6)  # Построение графика
+
+plt.xlabel('Дата')
+plt.ylabel('Значение')
+plt.title('График значений по датам')
+
+plt.xticks(rotation=45)
+
+plt.tight_layout()
+plt.show()
