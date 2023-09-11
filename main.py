2f6f86fc1c4ff03540d85c4b16a96f4c20105ba2
Волков Егор <egor.volkov20038@gmail.com>
2023-09-11 20:19:47 +0300

    fixed

--- main.py
+++ main.py
@@ -5,13 +5,15 @@ import matplotlib.pyplot as plt
 api_key = "ce8c8c95485e0c07bb277e1172325c78"
 
 print(requests.get(f"http://api.currencylayer.com/list?access_key={api_key}").json()['currencies'])
-
+time.sleep(2)
 first_request = requests.get(f"http://api.currencylayer.com/historical?"
                              f"access_key={api_key}&"
                              f"source=USD&"
                              f"currencies=EUR, GBP, JPY&"
                              f"date=2018-02-22")
 
+
+print(first_request.json())
 useful_data = first_request.json()['quotes']
 
 print(f"================================\n"
@@ -35,17 +37,17 @@ def generate_date():
     return date_list
 
 
-# course = {}
-# time.sleep(3)
-# for date in generate_data():
-#     second_request = requests.get(f"http://api.currencylayer.com/historical?"
-#                                   f"access_key={api_key}&"
-#                                   f"source=USD&"
-#                                   f"currencies=EUR&"
-#                                   f"date={str(date)}")
-#     print(second_request.json())
-#     time.sleep(1)
-#     course[date] = second_request.json()['quotes']['USDEUR']
+course = {}
+time.sleep(3)
+for date in generate_date():
+    second_request = requests.get(f"http://api.currencylayer.com/historical?"
+                                  f"access_key={api_key}&"
+                                  f"source=USD&"
+                                  f"currencies=EUR&"
+                                  f"date={str(date)}")
+    print(second_request.json())
+    time.sleep(1)
+    course[date] = second_request.json()['quotes']['USDEUR']
 
 dates = list(course.keys())
 values = list(course.values())
